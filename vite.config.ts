9291e61002417130c2632c6ce0412f13d62be927
iamisam <isamaziz256@gmail.com>
2025-09-30 15:48:20 +0530

    adding vite public api url

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  envPrefix: "VITE_",
+
   server: {
     proxy: {
       "/api": {
         target: "https://codeplay-backend-o9ve.onrender.com",
+        changeOrigin: true, // Needed for successful proxying
+        secure: false, // Use true if target is HTTPS, false if HTTP
       },
     },
   },
