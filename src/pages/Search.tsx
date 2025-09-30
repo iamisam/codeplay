bb8d38bc024abe03ebf6eeb80f98a6f8d60f2139
iamisam <isamaziz256@gmail.com>
2025-09-30 14:59:36 +0530

    stuff again

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { axiosPrivate } from "../api/axios"; // Use the public instance
+import { axiosPrivate } from "../api/axios";
 import { Link } from "react-router-dom";
 
 interface SearchResult {
